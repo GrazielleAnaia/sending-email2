7866ed693b80bb36d502369d7d88e492b635653e
Grazielle <grazielle.anaia@gmail.com>
2025-03-06 11:19:34 -0500

    initial commit

---
+++ src/main/java/com/grazielleanaia/sending_email2/SendingEmail2Application.java
@@ -0,0 +1,13 @@
+package com.grazielleanaia.sending_email2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendingEmail2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendingEmail2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grazielleanaia/sending_email2/SendingEmail2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grazielleanaia.sending_email2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendingEmail2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
